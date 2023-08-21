38b9c6c0efc38b37c58977b8f041309891fdb130
tony tong <tony.tong@interpublic.com>
2023-08-21 11:25:16 +0800

    add hr-dev

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "sandbox" {
 
   account_customizations_name = "sandbox-aft"
 }
+
+module "hr-dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jyong5054+hr_aft@gmail.com"
+    AccountName               = "hr-dev"
+    ManagedOrganizationalUnit = "HR"
+    SSOUserEmail              = "jyong5054@gmail.com"
+    SSOUserFirstName          = "Learn"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Created By" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "xutonggz@cn.ibm.com"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "hr-dev"
+}
